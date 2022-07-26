cecbe1ac55c320070851cf42b43db1916de86780
yuanjianhui <56376221+yuanjianhui@users.noreply.github.com>
2022-07-26 19:34:59 +0800

    Add files via upload

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'type' => 'mysql',
+    'host' => 'localhost',
+    'port' => '3306',
+    'dbname' => 'dxs',
+    'charset' => 'utf8',
+    'user' => 'root',
+    'pwd' => 'root',
+    'prefix' => 'dxs_'
+];
